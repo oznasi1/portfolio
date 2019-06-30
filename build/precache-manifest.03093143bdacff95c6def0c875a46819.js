c4fc09786100063f7fb6d413d80a33b3e45e40ae
oznasi1 <oznasi1@gmail.com>
2019-06-30 16:56:39 +0300

    build folder

---
+++ build/precache-manifest.03093143bdacff95c6def0c875a46819.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1773c4b99a7d02c16ddd8b0037cf4e7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "187a22e53d0d06efc511",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "5c092bb813acf855b146",
+    "url": "/static/css/main.b9d7cf15.chunk.css"
+  },
+  {
+    "revision": "187a22e53d0d06efc511",
+    "url": "/static/js/2.24c8edd5.chunk.js"
+  },
+  {
+    "revision": "5c092bb813acf855b146",
+    "url": "/static/js/main.31b4cc23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "254370afdfd4b4a1c376844a1feb4b61",
+    "url": "/static/media/Nexa-Bold.254370af.otf"
+  },
+  {
+    "revision": "729fc822f81d577e5d79a033a51c515e",
+    "url": "/static/media/Oz Nasi CV.729fc822.pdf"
+  },
+  {
+    "revision": "29a8a1a8489b578a0f6f987d57e39453",
+    "url": "/static/media/me.29a8a1a8.png"
+  },
+  {
+    "revision": "2fb957b9e77123922e1c556d64e7cf16",
+    "url": "/static/media/react_admin_pic.2fb957b9.png"
+  },
+  {
+    "revision": "984380aacfecb65121a73106257cd563",
+    "url": "/static/media/selenium_pic.984380aa.png"
+  },
+  {
+    "revision": "009cf691c9d02127943162a7328614b5",
+    "url": "/static/media/taki_pic.009cf691.png"
+  }
+]);
