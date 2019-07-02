4150168e72447e0d05dff4c0fc20ae9169dd878b
oznasi1 <oznasi1@gmail.com>
2019-07-02 16:15:10 +0300

    Updates

---
+++ precache-manifest.d6863de41a46efc6e617a0d13ad8723d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3df5330a2fd12b6d366282368f5fbd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2dbaaf127b877feac5e8",
+    "url": "./static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "dd3096fc41cbf467ba9d",
+    "url": "./static/css/main.f6e6111b.chunk.css"
+  },
+  {
+    "revision": "2dbaaf127b877feac5e8",
+    "url": "./static/js/2.3a3b18d7.chunk.js"
+  },
+  {
+    "revision": "dd3096fc41cbf467ba9d",
+    "url": "./static/js/main.11cb0c65.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "254370afdfd4b4a1c376844a1feb4b61",
+    "url": "./static/media/Nexa-Bold.254370af.otf"
+  },
+  {
+    "revision": "729fc822f81d577e5d79a033a51c515e",
+    "url": "./static/media/Oz Nasi CV.729fc822.pdf"
+  },
+  {
+    "revision": "1a3ed38e2b748367d62310e3fbbe56cc",
+    "url": "./static/media/led_screen_video.1a3ed38e.mp4"
+  },
+  {
+    "revision": "a0dc0523f2df0590c5eef5049048b7af",
+    "url": "./static/media/me.a0dc0523.png"
+  },
+  {
+    "revision": "5f160da02d6c4c8fcb9d7aac1310a4ae",
+    "url": "./static/media/react_admin_pic.5f160da0.png"
+  },
+  {
+    "revision": "64770dedfb79324933a4e7d099f3ec8b",
+    "url": "./static/media/react_admin_video.64770ded.mp4"
+  },
+  {
+    "revision": "f61dc2971d1c401d7daaab087dadd6ef",
+    "url": "./static/media/rise_up_alarm.f61dc297.png"
+  },
+  {
+    "revision": "888c2065120d770c4445983a530d0192",
+    "url": "./static/media/selenium_pic.888c2065.png"
+  },
+  {
+    "revision": "ba9125959fc5eeecb3abebe4ea8cd161",
+    "url": "./static/media/taki_pic.ba912595.png"
+  }
+]);
